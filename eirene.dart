1005301377e2f531620984abdcdb69605f9cbc4c
brussell98 <russellbrandon1999@gmail.com>
2016-10-16 17:12:25 -0500

    Added link blocking and word/phrase blacklist

--- eirene.dart
+++ eirene.dart
@@ -7,7 +7,7 @@ YamlMap config;
 
 Map<String, int> last_message = new Map();
 RegExp discord_link_regex = new RegExp(r'(discord\.gg\/[A-Za-z0-9]+|discordapp\.com\/invite\/[A-Za-z0-9]+)', caseSensitive: false);
-RegExp link_regex = new RegExp(r'https?:\/\/(?:\S+\.|(?![\s]+))[^\s\.]+\.[^\s]{2,}|\S+\.[^\s]+\.[^\s]{2,}', caseSensitive: false);
+RegExp link_regex = new RegExp(r'<?(https?:\/\/(?:\S+\.|(?![\s]+))[^\s\.]+\.[^\s]{2,}|\S+\.[^\s]+\.[^\s]{2,})>?', caseSensitive: false);
 
 main() async {
 	config = loadYaml(await new File('config.yaml').readAsString());
@@ -31,13 +31,41 @@ onMessage(MessageEvent e) {
 	if (config['whitelist'].contains(m.author.id) || !config['channels'].contains(m.channel.id))
 		return;
 
-	if (config['slow_mode']['enabled']) {
-		int sent = m.timestamp.millisecondsSinceEpoch;
-		if (last_message[m.author.id] != null && sent - last_message[m.author.id] < config['slow_mode']['time']) {
-			m.delete();
-			return;
-		}
+	if (config['slow_mode']['enabled'])
+		slowmodeCheck(m);
 
-		last_message[m.author.id] = sent;
+	if (config['links_block']['enabled'])
+		linkCheck(m);
+
+	if (config['blacklisted_words'].length != 0)
+		blacklistCheck(m);
+}
+
+slowmodeCheck(Message m) {
+	int sent = m.timestamp.millisecondsSinceEpoch;
+	if (last_message[m.author.id] != null && sent - last_message[m.author.id] < config['slow_mode']['time'])
+		return m.delete();
+
+	last_message[m.author.id] = sent;
+}
+
+linkCheck(Message m) {
+	if (discord_link_regex.hasMatch(m.content))
+		return m.delete();
+
+	if (!config['links_block']['invites_only']) {
+		String link = link_regex.stringMatch(m.content);
+		if (link != null) {
+			if (!config['links_block']['allow_non_embed'] || (!link.startsWith('<') && !link.endsWith('>')))
+				return m.delete();
+		}
+	}
+}
+
+blacklistCheck(Message m) {
+	String lower = m.content.toLowerCase();
+	for (String word in config['blacklisted_words']) {
+		if (lower.contains(word))
+			return m.delete();
 	}
 }
